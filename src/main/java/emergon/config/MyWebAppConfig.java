47391bde8d15d138d8a4d42629d1f0d694a15cb2
Anastasios Lelakis <tasos@hotmail.com>
2020-11-12 21:49:39 +0200

    initial commit

---
+++ src/main/java/emergon/config/MyWebAppConfig.java
@@ -0,0 +1,25 @@
+package emergon.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration//This class is the equivalent of dispatcher-servlet.xml
+@EnableWebMvc
+@ComponentScan(basePackages = "emergon")
+public class MyWebAppConfig implements WebMvcConfigurer {
+
+    
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/view/");
+        viewResolver.setSuffix(".jsp");
+        registry.viewResolver(viewResolver);
+    }
+
+    
+}
---
+++ src/main/java/emergon/config/MyWebSecurityApplicationInitializer.java
@@ -0,0 +1,7 @@
+package emergon.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+//Initializes the context to load Spring Security Configuration - Filters
+public class MyWebSecurityApplicationInitializer extends AbstractSecurityWebApplicationInitializer{
+    
+}
